a5bfe80c599ab7e54789907d414131b88bf7f20c
AleMonCayola <alejandrocayola19@gmail.com>
2023-09-27 16:09:31 -0400

    prueba

---
+++ spotify/src/components/busqueda.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Busqueda(){
+return(
+
+<div class="box">
+  <div class="container-1">
+      <span class="icon"><i class="fa fa-search"></i></span>
+      <input type="search" id="search" placeholder="¿Que estas buscando?" />
+  </div>
+</div>
+
+);
+}
+
+export default Busqueda;
