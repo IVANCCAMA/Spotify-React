7fc0a819f46b424a4e94936d567947124c94021f
daniel2010001 <129468193+daniel2010001@users.noreply.github.com>
2023-09-29 15:11:42 -0400

    eliminar de firebase si existe error

--- spotify/src/components/añadirCancion.js
+++ spotify/src/components/añadirCancion.js
@@ -1,68 +1,106 @@
 import React from 'react';
 import { useState } from 'react';
-import { SubirCancion } from '../firebase/config';
+import { SubirCancion, deleteFile } from '../firebase/config';
+
+const validarCampos = (campos) => {
+  if (campos.campo1 == "value1") {}
+  return true;
+}
+
+const validarFormatoArchivo = (archivo) => {
+  const formatosPermitidos = ["mpeg", "wav"]; // mpeg === mp3
+  for (const formato of formatosPermitidos) {
+    if (archivo.type.includes(formato)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const subirFirebase = async (archivo) => {
+  try {
+    const resultado = await SubirCancion(archivo);
+    return resultado;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+const subirBD = (archivo, url) => {
+  // code
+  return true;
+}
+
+const ValidarForm = async (e) => {
+  e.preventDefault();
+
+  // validar campos
+  const campos = {
+    campo1: "value1",
+    campo2: "value2"
+  };
+
+  if (!validarCampos(campos)) {
+    alert(`Error en los campos.`);
+    return;
+  }
+
+  // validar formato del archivo
+  const archivos = document.getElementById('archivo');
+  // se puede agregar varios archivos a la vez (por lote)
+  const archivo = archivos.files[0];
+
+  if (!validarFormatoArchivo(archivo)) {
+    alert(`Formato del archivo no admitido.`);
+    return;
+  }
+
+  try {
+    // subir el archivo a Firebase
+    const resultado = await SubirCancion(archivo);
+    alert(`Archivo subido correctamente.`);
+
+    // subir en la db
+    if (subirBD(campos, resultado)) {
+      // Si ocurre un error al subir en la base de datos
+      // eliminar el archivo subido en Firebase
+      deleteFile(resultado.filepath);
+      alert(`Archivo eliminado de Firebase.`);
+      return;
+    }
+
+    // Ventana para confirmar la subida
+
+    window.location.reload();
+  } catch (error) {
+    console.error('Error:', error);
+    alert(`Error al subir o procesar el archivo.`);
+  }
+};
 
 function AñadirCancion() {
-    const [file, setFile] = useState(null);
-    
-    const ValidarForm = async (e) => {
-        e.preventDefault();
-        // validar campos
-        // code
+  const [file, setFile] = useState(null);
 
-        // validar formato
-        const Formatos = ["mpeg", "wav"]; // mpeg === mp3
-        let flag = false;
+  return (
+    <div className="max-w-md mx-auto p-6 bg-white rounded-md shadow-md">
+      <h2 className="text-xl font-bold mb-4">Crear Lista</h2>
 
-        for (let i = 0; i < Formatos.length && !flag; i++) {
-            if (file.type.includes(Formatos[i])) {
-                flag = true;
-            }
-        }
-
-        if (flag) { 
-            // otra funcion (?)
-
-            // add en firebase
-            try {
-                const url = await SubirCancion(file);
-                console.log(url);
-                alert(`Archivo subido correctamente.`);
-            } catch (error) {
-                console.log(error);
-                alert(`Error al subir el archivo.`);
-            }
-
-            // add en db
-
-            // ventana para confirmar de subida
-            window.location.reload();
-
-        } else {
-            alert(`Formato del archivo no admitido.`);
-        }
-    };
-
-    return (
-        <div className="max-w-md mx-auto p-6 bg-white rounded-md shadow-md">
-            <h2 className="text-xl font-bold mb-4">Crear Lista</h2>
-    
-            <form onSubmit={ ValidarForm }>
-                <div className="mb-4">
-                    <label htmlFor="imagen" className="block text-gray-700">Seleccionar archivo con los formatos: mp3 o wav.</label>
-                    {/* <input type="file" className="w-full" name="archivo" id="archivo" onChange={e => SubirCancion(e.target.files[0])}/> */}
-                    {/* accept="image/jpeg, image/png" */}
-                    <input type="file" className="w-full" name="archivo" id="archivo" accept=".mp3, audio/wav" onChange={e => setFile(e.target.files[0])}/>
-                </div>
-    
-                <div className="flex justify-between">
-                    <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-md">Aceptar</button>
-                    <button type="button" className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md">Cancelar</button>
-                </div>
-            </form>
+      <form onSubmit={ValidarForm}>
+        <div className="mb-4">
+          <label htmlFor="imagen" className="block text-gray-700">Seleccionar archivo con los formatos: mp3 o wav.</label>
+          {/* <input type="file" className="w-full" name="archivo" id="archivo" onChange={e => SubirCancion(e.target.files[0])}/> */}
+          {/* accept="image/jpeg, image/png" */}
+          <input type="file" className="w-full" name="archivo" id="archivo" accept=".mp3, audio/wav" onChange={e => setFile(e.target.files[0])} />
         </div>
-    );
+
+        <div className="flex justify-between">
+          <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-md">Aceptar</button>
+          <button type="button" className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md">Cancelar</button>
+        </div>
+      </form>
+    </div>
+  );
 };
-  
+
 export default AñadirCancion;
 
