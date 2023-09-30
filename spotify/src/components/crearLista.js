330f1346fe026439844f4030da8dc15c6f07c2ca
IVANCCAMA <ccamaivan@gmail.com>
2023-09-30 01:57:59 -0400

    crearLista.js mejorado, add restricion maxima entrada

--- spotify/src/components/crearLista.js
+++ spotify/src/components/crearLista.js
@@ -1,53 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './crearLista.css'
 
 function CrearLista() {
 
-  // Form de lista
-const entradas = document.querySelectorAll('.validar');
+  const [titulo, setTitulo] = useState('');
+  const [artista, setArtista] = useState('');
 
-entradas.forEach(function (entrada) {
-    entrada.addEventListener('input', function(event) {
-        const valor = event.target.value;
-        
-        const alfanumerico = /^[a-zA-Z0-9]*$/;
-        if (!alfanumerico.test(valor)) {
-            entrada.classList.add('active');
-            // reemplazar el valor no alfanumerico
-            // event.target.value = valor.replace(/[^a-zA-Z0-9]/g, '');
-            console.log("valor ingresado no es alfanumerico");
-        } else {
-            entrada.classList.remove('active');
-        }
+  const handleTituloChange = (event) => {
+    const valor = event.target.value;
+    if (/^[a-zA-Z0-9]*$/.test(valor) && valor.length <= 20) {
+      setTitulo(valor);
+    }
+  };
 
-        if (valor.length > 20) {
-            // sub-string del valor hasta 20
-            // entrada.value = valor.slice(0, 20);
-            console.log("valor ingresado mayor a 20");
-        }
-    });
-});
-  
+  const handleArtistaChange = (event) => {
+    const valor = event.target.value;
+    if (/^[a-zA-Z0-9]*$/.test(valor) && valor.length <= 20) {
+      setArtista(valor);
+    }
+  };
+
   const handleSubirArchivo = () => {
     const imagen = document.getElementById('imagen');
-    imagen.click(); 
+    imagen.click();
   };
   return (
+      /* Form de lista */
     <div className="modal-crear-lista">
       <form className="modal-box">
         <div className="inter-modal">
           <div className="campo">
-            <div className="input-box">
+          <div className="input-box">
               <label htmlFor="titulo">Título del álbum *</label>
-              {/* Falta controlar entradas max 20 */}
-              <input type="text" className="validar" id="titulo" autoFocus required />
+              <input
+                type="text"
+                className="validar"
+                id="titulo"
+                autoFocus
+                value={titulo}
+                onChange={handleTituloChange} 
+                autofocus required
+              />
             </div>
           </div>
 
           <div className="campo">
             <div className="input-box">
               <label htmlFor="artista">Artista *</label>
-              <input type="text" className="validar" id="artista" autoFocus required />
+              <input
+                type="text"
+                className="validar"
+                id="artista"
+                autoFocus
+                value={artista}
+                onChange={handleArtistaChange}
+                autofocus required
+              />
             </div>
           </div>
 
