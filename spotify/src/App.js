71c06569b359711ef891aedcec315e82082b0fd2
IVANCCAMA <ccamaivan@gmail.com>
2023-09-29 20:48:39 -0400

    diseño home listo, mas form crearAlbum

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -3,15 +3,16 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import CrearLista from './components/crearLista';
 import AñadirCancion from './components/añadirCancion';
 import MenuLateral from './components/menuLateral';
+import './App.css'
 
 function App () {
   return (
-    <div className='bg-teal-300'> 
+    <div className='boby'> 
        <div className='flex'>
         <MenuLateral/>
           <div className="container mx-auto py-4 px-20">
             <Routes>
-              <Route path="/crearLista" element={< CrearLista />} />
+              <Route path="/crearAlbum" element={< CrearLista />} />
             </Routes>
             <Routes>
               <Route path="/añadirCancion" element={< AñadirCancion />} />
