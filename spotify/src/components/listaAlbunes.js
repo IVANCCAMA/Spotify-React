5652908ee22739ee990ca6d587489d8b5ec51546
danistyle <202108276@est.umss.edu>
2023-10-02 18:52:55 -0400

    lista canciones beta

--- spotify/src/components/listaAlbunes.js
+++ spotify/src/components/listaAlbunes.js
@@ -1,41 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import './listaAlbunes.css';
 import groupLogo from '../logos/group.png';
+import axios from "axios";
+/* import { Routes, Route } from 'react-router-dom';
+import ListaCanciones from "./listaCanciones"; */
+
+
 
 function ListaAlbumes() {
-  const albums = [
-    { id: 1, artist: "Nombre del Artista" },
-    { id: 2, artist: "Nombre del Artista" },
-    { id: 3, artist: "Nombre del Artista" },
-    { id: 4, artist: "Nombre del Artista" },
-    { id: 5, artist: "Nombre del Artista" },
-    { id: 6, artist: "Nombre del Artista" },
-    { id: 7, artist: "Nombre del Artista" },
-    { id: 8, artist: "Nombre del Artista" },
-    { id: 9, artist: "Nombre del Artista" }
-  ];
+   /*  const albums = [
+      { id: 1, artist: "Nombre del Artista" },
+      { id: 2, artist: "Nombre del Artista" },
+      { id: 3, artist: "Nombre del adsad" },
+      { id: 4, artist: "Nombre del Artista" },
+      { id: 5, artist: "Nombre del Artista" },
+      { id: 6, artist: "Nombre del Artista" },
+      { id: 7, artist: "Nombre del Artista" },
+      { id: 8, artist: "Nombre del Artista" },
+      { id: 9, artist: "Nombre del Artista" }
+    ];
+ */const [albumes, setAlbumes] = useState([]);
+ useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000/api/lista_canciones/');
+      const listaCanciones = response.data;
+      setAlbumes(listaCanciones);
+    } catch (error) {
+      console.error('Error al obtener la lista de canciones:', error);
+    }
+  };
 
-  return (
-    
-    
+  fetchData();
+}, []);
 
-    <div className="album-list">
-      
+return (
+  <div className="album-list">
+    {albumes.map((album, index) => (
+      <Link to={`/detalle-album/${album.id}`} key={album.id} className="album-item">
+        <img src={groupLogo} alt="Álbum" className="album-logo" />
+        <div className="album-details">
+          <div className="album-title">{album.titulo_lista}</div> {/* Reemplaza 'albumName' con el nombre real del atributo */}
+          <div className="artist-name">{album.colaborador}</div>
+          <div className="album-songs">{album.cantidad_canciones}</div> {/* Reemplaza 'id_lista' con el nombre real del atributo */}
+        </div>
+      </Link>
+    ))}
+  </div>
 
-      {albums.map((album, index) => (
-        <Link to={`/detalle-album/${album.id}`} key={album.id} className="album-item">
-          <img src={groupLogo} alt="Álbum" className="album-logo" />
-          <div className="album-details">
-            <div className="album-title">Album {index+1}</div>
-            <div className="artist-name">{album.artist}</div>
-            <div className="album-songs">0 canciones</div>
-          </div>
-        </Link>
-        
-      ))}
-      
-    </div>
 
     
 
