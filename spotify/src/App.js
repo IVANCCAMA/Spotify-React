e9fa004750b59f91ce42a2ce42cd13830cd936e9
danistyle <202108276@est.umss.edu>
2023-10-03 14:16:27 -0400

    Marco albumes v1

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import ListaAlbumes from './components/listaAlbunes';
 /* import Sencillo from './components/sencillo';
  */import Inicio from './components/inicioHome';
+ import ListaCanciones from './components/listaCanciones';
 
 
 function App() {
@@ -21,6 +22,9 @@ function App() {
             <Route path="/crearAlbum" element={<CrearLista />} />
             {/* <Route path="/Sencillo" element={<Sencillo />} /> */}
             <Route path="/añadirCancion" element={<AñadirCancion />} />
+
+{/*             <Route path={`/detalle-album/1`} element={<ListaCanciones />} />
+ */}
           </Routes>
         </div>
       </div>
