c9cc833392563e0116d589687afb862d2b09ac02
daniel2010001 <129468193+daniel2010001@users.noreply.github.com>
2023-10-06 07:29:56 -0400

    Bug asentos + anie Ñ

--- spotify/src/components/form.js
+++ spotify/src/components/form.js
@@ -1,18 +1,18 @@
 export const alfanumerico = (valor) => {
-  if (!/^[a-zA-Z0-9\s]*$/.test(valor)) {
+  if (!/^[a-zA-Z0-9\sáéíóúÁÉÍÓÚñÑ]*$/.test(valor)) {
     return false;
   }
   return true;
 };
 
 export const alfanumericoVarios = (valor) => {
-  if (!/^[a-zA-Z0-9\s,]*$/.test(valor)) {
+  if (!/^[a-zA-Z0-9\s,áéíóúÁÉÍÓÚñÑ]*$/.test(valor)) {
     return false;
-  } else if (/^[a-zA-Z0-9\s]*$/.test(valor)) {
+  } else if (/^[a-zA-Z0-9\sáéíóúÁÉÍÓÚñÑ]*$/.test(valor)) {
     return true;
   } else if (/,+[\s]*$/.test(valor)) {
     return false;
-  } else if (/[,a-zA-Z0-9\s]*$/.test(valor)) {
+  } else if (/[,a-zA-Z0-9\sáéíóúÁÉÍÓÚñÑ]*$/.test(valor)) {
     return true;
   }
   return false;
