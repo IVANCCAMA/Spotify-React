725d7364607dcf203a737bae2c5257e2a01b61e3
IVANCCAMAm <ccamaivan1@gmail.com>
2023-10-11 23:27:28 -0400

    bug centreado img inicio arreglado

--- spotify/src/components/inicioHome.js
+++ spotify/src/components/inicioHome.js
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from '../logos/logo.png';
+import './inicioHome.css'
 
 function Inicio() {
   return (
-    <div>
+    <div className="contenedor-imagen">
       <div className="logoInicio">
-          <img src={logo} alt="" width="500" />
+          <img src={logo} alt="" width="400" />
         </div>
     </div>
   );
