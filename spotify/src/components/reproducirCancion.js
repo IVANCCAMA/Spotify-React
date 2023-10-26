7d3e1811ed8ea28cc67cd91d5c816c31df9a0761
Edith Camacho <edith.teodocia@gmail.com>
2023-10-26 09:01:28 -0400

    reproduce automaticamente una musica de un album cuando termina

--- spotify/src/components/reproducirCancion.js
+++ spotify/src/components/reproducirCancion.js
@@ -1,5 +1,5 @@
 
-import React, { useState, useRef, useContext, useEffect } from "react";
+import React, { useState, useRef, useContext, useEffect, useCallback} from "react";
 import { FaPlay, FaPause, FaForward, FaBackward, FaVolumeOff, FaVolumeUp } from 'react-icons/fa';
 import './estilosReproductor.css';  
 import { ListContext } from "./ListContext";
@@ -18,37 +18,37 @@ function ReproducirCancion () {
   const [progreso, setProgreso] = useState(0);
   const [muted, setMuted] = useState(false);  // Mute - Unmuted
   
+ 
+  
+
   // Recupera cancion selecionada de ListaCanciones.js
   useEffect(() => {
     if (cancionSeleccionada) {
-        setCancionSelect(cancionSeleccionada);
-
-        // Actualización de los nombres
-        setNombreArtista(cancionSeleccionada.nombre_usuario);
-        setNombreMusica(cancionSeleccionada.nombre_cancion);
-
-        const audio = audioRef.current;
-        audio.src = cancionSeleccionada.path_cancion; // Establecemos la fuente de la canción
-        audio.play().then(() => {  // Inicia la reproducción de la canción automáticamente
-            setEstaReproduciendo(true);
-        });
-
-        audio.addEventListener('timeupdate', () => {
-            const porcentaje = (audio.currentTime / audio.duration) * 100;
-            setProgreso(porcentaje);
-        });
-        const handleTimeUpdate = () => {
-          const porcentaje = (audio.currentTime / audio.duration) * 100;
-          setProgreso(porcentaje);
+      setCancionSelect(cancionSeleccionada);
+  
+      // Actualización de los nombres
+      setNombreArtista(cancionSeleccionada.nombre_usuario);
+      setNombreMusica(cancionSeleccionada.nombre_cancion);
+  
+      const audio = audioRef.current;
+      audio.src = cancionSeleccionada.path_cancion; // Establecemos la fuente de la canción
+      audio.play().then(() => {  // Inicia la reproducción de la canción automáticamente
+        setEstaReproduciendo(true);
+      });
+  
+      const handleTimeUpdate = () => {
+        const porcentaje = (audio.currentTime / audio.duration) * 100;
+        setProgreso(porcentaje);
       };
-      
+  
       audio.addEventListener('timeupdate', handleTimeUpdate);
       
       return () => {
-          audio.removeEventListener('timeupdate', handleTimeUpdate);
+        audio.removeEventListener('timeupdate', handleTimeUpdate);
       };
     }
   }, [cancionSeleccionada]);
+  
 
 
 
@@ -72,7 +72,7 @@ function ReproducirCancion () {
         console.error('audioRef.current no está definido o no hay una canción seleccionada');
     }
   };
-  const sigCancion = () => {
+  const sigCancion = useCallback(() => {
     let newIndex = indiceCancionActual + 1;
     if (newIndex >= listaCancionesReproduccion.length) {
         newIndex = 0;  // Si es la última canción, vuelve a la primera.
@@ -84,8 +84,28 @@ function ReproducirCancion () {
     
     setIndiceCancionActual(newIndex);
     cargarCancion(newIndex);
-};
+  }, [indiceCancionActual, listaCancionesReproduccion]);
+  
+  useEffect(() => {
+    const audio = audioRef.current;
 
+    const handleTimeUpdate = () => {
+      const porcentaje = (audio.currentTime / audio.duration) * 100;
+      setProgreso(porcentaje);
+    };
+
+    const handleEnded = () => {
+      sigCancion();
+    };
+
+    audio.addEventListener('timeupdate', handleTimeUpdate);
+    audio.addEventListener('ended', handleEnded);
+
+    return () => {
+      audio.removeEventListener('timeupdate', handleTimeUpdate);
+      audio.removeEventListener('ended', handleEnded);
+    };
+  }, [sigCancion]);
 
   const cargarCancion = (indice) => {
     const cancion = listaCancionesReproduccion[indice];
