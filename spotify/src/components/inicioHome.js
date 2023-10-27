d17d5d7619a1cba7a8af33dae8a08097a4a1a69e
IVANCCAMA <ccamaivan@gmail.com>
2023-10-27 00:48:09 -0400

    Fib ok ID: SSDM - 378

--- spotify/src/components/inicioHome.js
+++ spotify/src/components/inicioHome.js
@@ -4,10 +4,15 @@ import './inicioHome.css'
 
 function Inicio() {
   return (
-    <div className="contenedor-imagen">
-      <div className="logoInicio">
-          <img src={logo} alt="" width="400" />
+    <div>
+      <header className="header">
+        <button className='boton-registro'>Regístrate</button>  
+      </header>
+      <div className="contenedor-imagen">
+        <div className="logoInicio">
+            <img src={logo} alt="" width="400" />
         </div>
+      </div>
     </div>
   );
 }
