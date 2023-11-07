accb7f945fbe5fd7046b57a4139e14f3c6445735
Edith Camacho <edith.teodocia@gmail.com>
2023-11-07 19:45:12 -0400

    Agregar boton iniciar sesion

--- spotify/src/components/encabezado.js
+++ spotify/src/components/encabezado.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 function Encabeazado() {
     return (
         <header className="header">
+            <Link  to="/iniciarsesion" className='boton-iniciarsesion'><strong>Iniciar Sesión</strong></Link> 
             <Link  to="/registro" className='boton-registro'><strong>Regístrate</strong></Link>  
             <Link to="/perfil">
                 <Icon icon="gg:profile" color="white" width="65" height="65" />
