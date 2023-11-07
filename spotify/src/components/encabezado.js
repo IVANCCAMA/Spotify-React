97f3931822a4d259a5ee14e8c58fb8541bcdce68
IVANCCAMA <ccamaivan@gmail.com>
2023-11-07 11:21:42 -0400

    add encabezado estarico en conponentes

---
+++ spotify/src/components/encabezado.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Icon } from '@iconify/react';
+import { Link } from "react-router-dom";
+
+function Encabeazado() {
+    return (
+        <header className="header">
+            <Link  to="/registro" className='boton-registro'><strong>Regístrate</strong></Link>  
+            <Link to="/perfil">
+                <Icon icon="gg:profile" color="white" width="65" height="65" />
+            </Link>
+        </header>
+    );
+}
+
+export default Encabeazado;
--- spotify/src/components/inicioHome.js
+++ spotify/src/components/inicioHome.js
@@ -1,18 +1,12 @@
 import React from "react";
 import logo from '../logos/logo.png';
 import './inicioHome.css';
-import { Icon } from '@iconify/react';
-import { Link } from "react-router-dom";
+import Encabeazado from "./encabezado";
 function Inicio() {
   return (
     // <div>
     <>
-      <header className="header">
-        <Link  to="/registro" className='boton-registro'><strong>Regístrate</strong></Link>  
-        <Link to="/perfil">
-          <Icon icon="gg:profile" color="white" width="65" height="65" />
-        </Link>
-      </header>
+      
       <div className="contenedor-imagen">
         <div className="logoInicio">
           <img src={logo} alt="" width="400" />
