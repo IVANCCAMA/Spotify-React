d9570fdb2ba99fdb322f4972171df1b8e786684a
IVANCCAMA <ccamaivan@gmail.com>
2023-11-08 15:24:56 -0400

    add menu desplazable + funcionalidad

--- spotify/src/components/encabezado.js
+++ spotify/src/components/encabezado.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon } from '@iconify/react';
 import { Link } from "react-router-dom";
 import "./encabezado.css"
 
-function Encabeazado() {
+function Encabezado() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };  
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
-        <header className="header">
-            <Link  to="/registro" className='boton-registro'><strong>Regístrate</strong></Link>  
-            <Link to="/perfil">
-                <Icon icon="gg:profile" color="white" width="45" height="45" />
-            </Link>
-        </header>
+        <div>
+            <header className="header">
+                <Link to="/registro" className='boton-registro'><strong>Regístrate</strong></Link>
+                <button onClick={toggleMenu}>
+                    <Icon icon="gg:profile" color="white" width="45" height="45" />
+                </button>
+                {isMenuOpen && (
+                    <div className="menu-options">
+                        <Link to="/perfil" onClick={closeMenu}>Perfil</Link>
+                        <Link to="/" onClick={closeMenu}>Cerrar sesión</Link>
+                    </div>
+                )}
+            </header>
+        </div>
     );
 }
 
-export default Encabeazado;
+export default Encabezado;
