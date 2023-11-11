190178dcf03b2372f50631ea2654a807890873af
daniel2010001 <129468193+daniel2010001@users.noreply.github.com>
2023-11-10 22:17:42 -0400

    Bug color encabesado

--- spotify/src/components/encabezado.js
+++ spotify/src/components/encabezado.js
@@ -4,40 +4,38 @@ import { Link, useLocation } from "react-router-dom"; // Importar useLocation
 import "./encabezado.css"
 
 function Encabezado() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const location = useLocation(); // Obtener la ubicación actual
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    // Es si la ruta actual es 'iniciarsesion' o 'registro'
-    const isAuthRoute = location.pathname === '/iniciarsesion' || location.pathname === '/registro';
+  const isAuthRoute = location.pathname === '/iniciarsesion' || location.pathname === '/registro';
 
-    return (
-        <div>
-            <header className="header">
-                {/* Mostrar los botones solo si no estamos en una ruta de autenticación */}
-                {!isAuthRoute && (
-                    <>
-                        <Link to="/iniciarsesion" className='boton-iniciarsesion'><strong>Iniciar Sesión</strong></Link>
-                        <Link to="/registro" className='boton-registro'><strong>Regístrate</strong></Link>
-                    </>
-                )}
-                  {!isAuthRoute && (
-                <button onClick={toggleMenu}>
-                    <Icon icon="gg:profile" color="white" width="45" height="45" />
-                </button>
-                 )}
-                {isMenuOpen && (
-                    <div className="menu-options">
-                        <Link to="/perfil">Perfil</Link>
-                        <Link to="/">Cerrar sesión</Link>
-                    </div>
-                )}
-            </header>
-        </div>
-    );
+  return (
+    <div>
+      <header className="header-Encabesado">
+        {!isAuthRoute && (
+          <>
+            <Link to="/iniciarsesion" className='boton-iniciarsesion'><strong>Iniciar Sesión</strong></Link>
+            <Link to="/registro" className='boton-registro'><strong>Regístrate</strong></Link>
+          </>
+        )}
+        {!isAuthRoute && (
+          <button onClick={toggleMenu}>
+            <Icon icon="gg:profile" color="white" width="45" height="45" />
+          </button>
+        )}
+        {isMenuOpen && (
+          <div className="menu-options">
+            <Link to="/perfil">Perfil</Link>
+            <Link to="/">Cerrar sesión</Link>
+          </div>
+        )}
+      </header>
+    </div>
+  );
 }
 
 export default Encabezado;
