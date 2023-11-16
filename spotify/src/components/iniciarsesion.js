50ab29ad256afdcbadf3c0ec7ae762d577baae2f
IVANCCAMAm <ccamaivan1@gmail.com>
2023-11-15 22:10:07 -0400

    fix funciones que no se usan

--- spotify/src/components/iniciarsesion.js
+++ spotify/src/components/iniciarsesion.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { VscEye, VscEyeClosed } from "react-icons/vsc";
 import { Link, useNavigate } from "react-router-dom";
 import './form.css';
