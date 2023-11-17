61e51f469959939ecc7e640bda0ff124b8c51859
AleMonCayola <alejandrocayola19@gmail.com>
2023-11-16 23:35:00 -0400

    bugs corregidos

--- spotify/src/components/menuLateral.js
+++ spotify/src/components/menuLateral.js
@@ -43,7 +43,7 @@ function MenuLateral({ userType, isLogin, showAlertModal }) {
       const requiresAuthentication = menuOption.to === '/crearListaReproduccion' || menuOption.to === '/biblioteca';
       if (requiresAuthentication) {
         e.preventDefault();
-        showAlertModal('Funcionalidad no permitida. Por favor, inicie sesión', "/");
+        showAlertModal('Funcionalidad no permitida. Por favor, inicie sesión');
       }
     }
   };
