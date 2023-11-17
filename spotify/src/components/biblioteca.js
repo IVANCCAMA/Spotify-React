55f442dc9c89f8087bf598f6488e71d196f68073
daniel2010001 <129468193+daniel2010001@users.noreply.github.com>
2023-11-17 13:05:39 -0400

    Estilos y refac de la db en perfil y biblioteca

--- spotify/src/components/biblioteca.js
+++ spotify/src/components/biblioteca.js
@@ -1,45 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
 import axios from "axios";
- 
+import ListaAlbumes from './listaAlbunes';
+import './albumes.css';
+
 function Biblioteca({ userConnected }) {
+  const database = 'https://spfisbackend-production.up.railway.app/api';
   const [albumes, setAlbumes] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`https://spfisbackend-production.up.railway.app/api/lista_canciones/oyente/${userConnected.id_usuario}`);
-        const listaCanciones = response.data;
-        listaCanciones.sort((a, b) => {
-          return a.titulo_lista.localeCompare(b.titulo_lista);
-        });
 
-        setAlbumes(listaCanciones);
-      } catch (error) {
-        console.error('Error al obtener la lista de canciones:', error);
-      }
-    };
+  const fetchData = async () => {
+    try {
+      const query = `/lista_canciones/oyente/${userConnected.id_usuario}`;
+      const response = await axios.get(`${database}${query}`);
+      const listaCanciones = response.data;
+      listaCanciones.sort((a, b) => {
+        return a.titulo_lista.localeCompare(b.titulo_lista);
+      });
 
-    fetchData();
-  }, [userConnected]);
+      setAlbumes(listaCanciones.slice(0, 6));
+    } catch (error) {
+      console.error('Error al obtener la lista de canciones:', error);
+    }
+  };
+
+  fetchData();
 
   return (
-    <div className='albun-content'/* style={{gridTemplateColumns: 'repeat(3, minmax(300px, 1fr))'}} */>
-    <div className="albumes-list" >
-      {albumes.map((album, index) => (
-        <Link to={`/lista-canciones-user/${album.id_lista}`} key={album.id_lista} className="albumes-item">
-          <img
-            src={album.path_image}
-            alt="Álbum"
-            className="albumes-image"
-          />
-          <div className="albumes-details">
-            <div className="albumes-title">{album.titulo_lista}</div>
-            <div className="artistas-name">{album.nombre_usuario}</div>
-            <div className="albumes-songs">{album.cantidad_canciones} canciones</div>
-          </div>
-        </Link>
-      ))}
-    </div>
+    <div className='album-content'>
+      <span className='albums-title'>Listas de reproducción</span>
+      <ListaAlbumes albumes={albumes} />
     </div>
   );
 }
